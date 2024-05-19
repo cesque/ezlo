af9da51f0e85a9d60fdd08995a4e57ab46058224
cesque <euqsec@gmail.com>
2024-05-19 13:32:18 +0100

    add icon

--- src/renderer/src/components/Header/Header.tsx
+++ src/renderer/src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
 import styles from './Header.module.css'
 
+import {Coolshape} from 'coolshapes-react'
+
 import CloseIcon from '@renderer/assets/icons/close.svg?react'
 import useApi from '@renderer/hooks/useApi'
 
@@ -7,7 +9,15 @@ export default function Header() {
     const { close } = useApi()
 
     return <div className={styles.header}>
-        <h1 className={styles.title}>Ezlo</h1>
+        <h1 className={styles.title}>
+            <Coolshape
+                type="triangle"
+                index={4}
+                noise={true}
+                size={30}
+            />
+            Ezlo
+        </h1>
 
         <button type="button" className={styles.closeButton} onClick={close}>
             <CloseIcon />
